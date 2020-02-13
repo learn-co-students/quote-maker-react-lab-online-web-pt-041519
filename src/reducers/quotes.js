938296b9e8d9b0389908aafea3ab46f69292daa7
earthctzn <earthctzn1@gmail.com>
2020-02-13 17:19:21 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,38 @@
 export default (state = [], action) => {
-  return state;
+  let idx
+  let qut
+  switch (action.type) {
+    case 'ADD_QUOTE':
+
+      return state.concat(action.quote)
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id  === action.quoteId)
+      qut = state[idx]
+      console.log('upvote', action.quoteId)
+      return [
+        ...state.slice(0, idx), 
+        Object.assign({}, qut, {votes: (qut.votes += 1)}), 
+        ...state.slice(idx + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id  === action.quoteId)
+      qut = state[idx]
+      console.log('downvote', action.quoteId)
+      if(qut.votes > 0) {
+        return [
+          ...state.slice(0, idx), 
+          Object.assign({}, qut, {votes: (qut.votes -= 1)}), 
+          ...state.slice(idx + 1)
+        ];
+      }
+      return state
+
+  default:
+    return state;
+  }    
 }
